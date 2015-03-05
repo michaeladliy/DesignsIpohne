15cfe131fb056d3e66e950903f09021073587c85
Michael Adliy <michael@Michaels-MacBook-Pro.local>
2015-03-05 18:48:08 +0200

    Create Application

---
+++ DesignsApplication/DesignsApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DesignsApplication
+//
+//  Created by Michael Adliy on 3/5/15.
+//  Copyright (c) 2015 Michael Adliy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DesignsApplication/DesignsApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DesignsApplication
+//
+//  Created by Michael Adliy on 3/5/15.
+//  Copyright (c) 2015 Michael Adliy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
